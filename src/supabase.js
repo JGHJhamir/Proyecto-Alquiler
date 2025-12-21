59ea022a8dbc56f2077ec726f79eaca7a1cc6b5a
JGHJhamir <jghjhamir11@gmail.com>
2025-12-21 16:32:51 -0500

    feat: pagina hola mundo simple

---
+++ src/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+// Estas variables las obtendrás de tu panel de Supabase
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
