ec1a7168b5926873b49e7bb85b414add58b993b5
JGHJhamir <jghjhamir11@gmail.com>
2025-12-23 20:35:24 -0500

    feat: add red color to text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '3rem', fontFamily: 'sans-serif' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '3rem', fontFamily: 'sans-serif', color: 'red' }}>
       <h1>Hola Mundo</h1>
     </div>
   )
